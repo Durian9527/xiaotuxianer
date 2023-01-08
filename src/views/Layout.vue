77a3d10b399491ffa02b805fbb2dcd5e6cd691e2
Durian <AbigDurian9527@163.com>
2023-01-08 17:02:58 +0800

    吸顶头部完成

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -2,6 +2,7 @@
     <div>
         <AppNavBar></AppNavBar>
         <AppHeader></AppHeader>
+        <AppHeaderSticky></AppHeaderSticky>
         <div class="app-body">
             <RouterView></RouterView>
         </div>
@@ -13,13 +14,15 @@
 import AppNavBar from '@/components/app-navbar.vue'
 import AppHeader from '@/components/app-header.vue'
 import AppFooter from '@/components/app-footer.vue'
+import AppHeaderSticky from '@/components/app-header-sticky.vue'
 import { useStore } from 'vuex'
 export default {
   name: 'xtx-Layout',
   components: {
     AppNavBar,
     AppHeader,
-    AppFooter
+    AppFooter,
+    AppHeaderSticky
   },
   setup () {
     const store = useStore()
